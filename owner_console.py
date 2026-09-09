03896906aa26868b964af3b995c19be50bbedb4f
willy612 <willy@nearcal.com>
2026-09-09 06:50:02 -0700

    Update owner_console.py

--- owner_console.py
+++ owner_console.py
@@ -13,7 +13,7 @@ from html import escape
 from fastapi import Form
 from fastapi.responses import HTMLResponse, RedirectResponse, JSONResponse
 
-OWNER_CONSOLE_VERSION = "7.4.8"
+OWNER_CONSOLE_VERSION = "7.4.9"
 OWNER_EMAIL = "buildcommandai@gmail.com"
 
 
