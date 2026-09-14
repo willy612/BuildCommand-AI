a75a4ab4f690179c06f474c7bc4f15ee42308ff1
willy612 <willy@nearcal.com>
2026-09-14 14:37:53 -0700

    Update full_app.py

--- full_app.py
+++ full_app.py
@@ -62243,3 +62243,11 @@ _bc8160_field = _bc8160_field_install(globals())
 BUILD_COMMAND_RELEASE = '8.16.0'
 BUILD_COMMAND_RELEASE_NAME = 'Drawings to Field'
 app.version = BUILD_COMMAND_RELEASE
+
+
+# 8.17.0: drawing locations connected to real job records and reviewed actions.
+from drawing_work import install as _bc8170_work_install
+_bc8170_work = _bc8170_work_install(globals())
+BUILD_COMMAND_RELEASE = '8.17.0'
+BUILD_COMMAND_RELEASE_NAME = 'Drawing Work Cards'
+app.version = BUILD_COMMAND_RELEASE
